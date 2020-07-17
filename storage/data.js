786662a619eb2cd8573e5542629645e727ba4baa
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-07-16 21:31:58 -0500

    pushing backend

---
+++ storage/data.js
@@ -0,0 +1,287 @@
+HotelRoom.create([
+  {
+    name: "single-economy",
+    slug: "single economy",
+    roomType: "single",
+    price: 100,
+    size: 200,
+    capacity: 1,
+    pets: false,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "single basic",
+    slug: "single-basic",
+    roomType: "single",
+    price: 150,
+    size: 250,
+    capacity: 1,
+    pets: false,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "single standard",
+    slug: "single-standard",
+    roomType: "single",
+    price: 250,
+    size: 300,
+    capacity: 1,
+    pets: true,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "single deluxe",
+    slug: "single-deluxe",
+    roomType: "single",
+    price: 300,
+    size: 400,
+    capacity: 1,
+    pets: true,
+    breakfast: true,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "double economy",
+    slug: "double-economy",
+    roomType: "double",
+    price: 200,
+    size: 300,
+    capacity: 2,
+    pets: false,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "double basic",
+    slug: "double-basic",
+    roomType: "double",
+    price: 250,
+    size: 350,
+    capacity: 2,
+    pets: false,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "double standard",
+    slug: "double-standard",
+    roomType: "double",
+    price: 300,
+    size: 400,
+    capacity: 2,
+    pets: true,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "double deluxe",
+    slug: "double-deluxe",
+    roomType: "double",
+    price: 400,
+    size: 500,
+    capacity: 2,
+    pets: true,
+    breakfast: true,
+    featured: true,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "family economy",
+    slug: "family-economy",
+    roomType: "family",
+    price: 300,
+    size: 500,
+    capacity: 3,
+    pets: false,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "family basic",
+    slug: "family-basic",
+    roomType: "family",
+    price: 350,
+    size: 550,
+    capacity: 4,
+    pets: false,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "family standard",
+    slug: "family-standard",
+    roomType: "family",
+    price: 400,
+    size: 600,
+    capacity: 5,
+    pets: true,
+    breakfast: false,
+    featured: false,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+  {
+    name: "family deluxe",
+    slug: "family-deluxe",
+    roomType: "family",
+    price: 500,
+    size: 700,
+    capacity: 6,
+    pets: true,
+    breakfast: true,
+    featured: true,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+
+  {
+    name: "presidential",
+    slug: "presidential-room",
+    roomType: "presidential",
+    price: 600,
+    size: 1000,
+    capacity: 10,
+    pets: true,
+    breakfast: true,
+    featured: true,
+    description:
+      "The hotel rooms and suites at the Zilnai offer the perfect blend of beauty and elegance. Our rooms will elevate your stay, whether you're traveling for business, celebrating a honeymoon, or just looking for a remarkable getaway. With views of the Las Vegas valley or the iconic Zilnai fountains right from your suite, your stay will be nothing short of unforgettable.",
+  },
+]);
+Image.create([
+  {
+    roomImage: "images/room-1.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-2.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-3.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-4.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-5.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-6.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-7.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-8.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-9.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-10.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-11.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-12.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+  {
+    roomImage: "images/room-13.jpeg",
+    roomDetails1: "images/details-1.jpeg",
+    roomDetails2: "images/details-2.jpeg",
+    roomDetails3: "images/details-3.jpeg",
+    roomDetails4: "images/details-4.jpeg",
+  },
+]);
+
+Extra.create([
+  {
+    extras1: "Plush pillows and breathable bed linens",
+    extras2: "Soft, oversized bath towels",
+    extras3: "Full-sized, pH-balanced toiletries",
+    extras4: "Complimentary refreshments",
+    extras5: "Adequate safety/security",
+    extras6: "Internet",
+    extras7: "Comfortable beds",
+  },
+]);
